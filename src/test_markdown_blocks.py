4581a87a296fa45a054b1efb9918cfe2f36110b0
Tasuba <Brokensky585@gmail.com>
2025-07-23 04:39:02 -0400

    Revert "page wasn't deploying, refactored and hopefully fixed"
    This reverts commit 0b43aaf77a4d18977b4ca69167183cb148aa24f6.

--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -151,18 +151,16 @@ this is paragraph text
     def test_codeblock(self):
         md = """
 ```
-This is a code block
+This is text that _should_ remain
+the **same** even with inline stuff
 ```
-
-this is paragraph text
-
 """
 
         node = markdown_to_html_node(md)
         html = node.to_html()
         self.assertEqual(
             html,
-            "<div><pre><code>This is a code block\n</code></pre><p>this is paragraph text</p></div>",
+            "<div><pre><code>This is text that _should_ remain\nthe **same** even with inline stuff\n</code></pre></div>",
         )
 
 
